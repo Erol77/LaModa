6e8ec11795844e59305d8e0efc97dcdaa92068a4
Erol <veliev77@mail.ru>
2021-07-07 17:45:46 +0300

    added function nou work

--- js/script.js
+++ js/script.js
@@ -60,9 +60,6 @@ const getGoods = (callback) => {
         });
 };
 
-getGoods((data) => {
-    console.warn(data);
-});
 subheaderCart.addEventListener('click',cartModalOpen);
 
 cartOverlay.addEventListener('click',event => {
@@ -74,8 +71,58 @@ cartOverlay.addEventListener('click',event => {
 
 } );
 
+try {
+        const goodsList = document.querySelector('.goods__list');
+            if (!goodsList ) {throw 'this is not a goods page!'; 
+        }
+        const createCard = ({ id, preview, cost, brand, name, sizes  }) => {
+            const li = document.createElement('li');
+            li.classList.add('goods__item');
 
+            li.innerHTML = `
+            <article class="good">
+                <a class="good__link-img" href="card-good.html#${id}">
+                    <img class="good__img" src="goods-image/${preview}" alt="">
+                </a>
+                <div class="good__description">
+                    <p class="good__price">${cost} &#8381;</p>
+                    <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+                    ${sizes ? `
+                    <p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>
+                    ` : '' }
+                     <a class="good__link" href="card-good.html#id56454">Подробнее</a>
+                </div>
+            </article>
+            `;
 
+            return li;
+        };
+        const renderGoodsList = data => {
+            goodsList.textContent = '';
+         /*   console.log('figa',data);
+            for (let i = 0; i < data.length ; i++){
+                console.log(data[i]);
+                console.log('----');
+            }
+            for (const item of data){
+                console.log('for/of',item);
+            }
+         */   data.forEach((item,i,arr) => {
+                const card = createCard(item);
+                goodsList.append(card);
+            });
+        };
+        getGoods(renderGoodsList);
+    
+}catch (err) {
+        console.warn(err);
+}
+
+/*выводит в консоль все товары 
+    getGoods((data) => {
+        console.warn(data);
+    });
+*/
 
 /* метод асиннхронного позволяет с помощью await загрузить после загрузки данных
 console.log(getData());
